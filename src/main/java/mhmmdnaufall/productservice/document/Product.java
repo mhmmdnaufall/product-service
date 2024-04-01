74d3a56cf36492c42e3eac8ec288fb55b8bddd47
Muhammad Naufal <muhammadnaufaall@gmail.com>
2024-04-01 14:13:43 +0700

    Create Product document

---
+++ src/main/java/mhmmdnaufall/productservice/document/Product.java
@@ -0,0 +1,26 @@
+package mhmmdnaufall.productservice.document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+
+@Document("products")
+@Data @Builder
+@NoArgsConstructor @AllArgsConstructor
+public class Product {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+}
